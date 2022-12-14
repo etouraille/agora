8d82b64fa1bd96a694a92f7bf184c375b6d43478
etouraille <eouard.touraille@gmail.com>
2022-12-14 06:38:17 +0100

    fix

--- node/socket.js
+++ node/socket.js
@@ -1,6 +1,13 @@
 const WebSocket = require('ws');
 const {socketDocument} = require("./socket/document");
 
+Array.prototype.max = function() {
+    return Math.max.apply(null, this);
+};
+
+Array.prototype.min = function() {
+    return Math.min.apply(null, this);
+};
 
 
 const wss = new WebSocket.Server({ port: 8081 });
