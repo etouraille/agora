9a22b20a325c90634483c45b4bd2f44a19a45c25
etouraille <eouard.touraille@gmail.com>
2022-12-18 09:52:36 +0100

    co

--- node/api/mercure.js
+++ node/api/mercure.js
@@ -3,7 +3,7 @@ const config = require('./../config');
 const mercure = (req, res ) => {
 
     let token = jwt.sign({ mercure : {  subscribe : []}}, config.mercureSubscriberToken, {
-        expiresIn: 3600*24, // Bearer expiring in one minute
+        expiresIn: 3600*24*10, // Bearer expiring in one minute
         noTimestamp: true, // Do not add "issued at" information to avoid error "Token used before issued"
         algorithm: 'HS256',
     });
